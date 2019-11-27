a0e294175ffd44e544d3ab86177c1e830ff96ad1
hassan_monfared <hasandoulabi@gmail.com>
2019-11-27 17:56:35 +0330

    add class && construct && function render for send view &&sen data in view

--- libs/View.php
+++ libs/View.php
@@ -1,4 +1,17 @@
 <?php
-class View{
+class View
+{
+    public function __construct()
+    {
 
+    }
+    public function render($name, $param = array())
+    {
+
+        extract($param);
+        require "Views/layout/header.php";
+        require "Views//$name.php";
+        require "Views/layout/footer.php";
+        return;
+    }
 }
