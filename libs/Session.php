d56bf7b2d3a947deedc1d177d5b1e45271f2bd3e
hassan_monfared <hasandoulabi@gmail.com>
2019-11-28 21:54:43 +0330

    add claas for Session

---
+++ libs/Session.php
@@ -0,0 +1,27 @@
+<?php
+
+class Session
+{
+
+    public static function init()
+    {
+        session_start();
+
+    }
+
+    public static function setSession($name, $param)
+    {
+        $_SESSION[$name] = $param;
+    }
+
+    public static function getSession($name)
+    {
+
+        return $_SESSION[$name];
+
+    }
+    public static function sessionDeath(){
+        session_unset();
+        session_destroy();
+    }
+}
