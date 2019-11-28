374bff18a929ebe1bf3213c212f7ed78e549fe03
hassan_monfared <hasandoulabi@gmail.com>
2019-11-28 21:59:40 +0330

    function for check user in data base

--- Controllers/user.php
+++ Controllers/user.php
@@ -7,10 +7,32 @@ public $mymodel;
         parent::__construct();
         $this->mymodel=$model;
     }
+    // show form login
     public function login()
     {
        
         $this->viewObject->render('user/login');
 
     }
+    // checkPOSt data
+    public function checklogin()
+    {
+        $email=$_POST['email'];
+        $pass=$_POST['password'];
+        if(isset($pass,$email)){
+            Session::init();setSession();
+            // Session::
+        }
+        $fetch = $this->mymodel->select('users', "*", "email = '$email' AND password = '$pass'");
+        if ($fetch){
+            Session::init();
+            Session::setSession('Login', $email);
+            header('Location: '.URL."/home/index/");
+        }else{
+            header('Location: '.URL."/user/login/");
+        }
+
+        return;
+    }
+    
 }
