882c9b09507199638bb257f8b38a333f898d036e
hassan_monfared <hasandoulabi@gmail.com>
2019-11-28 19:01:31 +0330

    add src script // src css in headr footer //

--- Views/layout/footer.php
+++ Views/layout/footer.php
@@ -1,2 +1,5 @@
+<script src="<?=URL."/libs/assest/js/jquery-3.4.1.min.js"?>"></script>
+<script src="<?=URL."/libs/assest/bootstrap/js/bootstrap.min.js"?>"></script>
+
 </body>
 </html>
--- index.php
+++ index.php
@@ -6,8 +6,10 @@ require "vendor/autoload.php";
 $dotenv = Dotenv\Dotenv::create(__DIR__);
 $dotenv->load();
 
-
-
+// add url
+define('URL', $_SERVER["REQUEST_SCHEME"] . "://" . $_SERVER["HTTP_HOST"] . "/My-Mini-Mvc");// . $_SERVER["REQUEST_URI"]);
+define('URL_JS', '/libs/asstes/js');
+define('URL_CSS', '/libs/asstes/css');
 // requrired base file
 require_once "libs/Model.php";
 require_once "libs/View.php";
