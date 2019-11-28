d41d6777c3c69ebc5d5b606901caedf051f4ee35
hassan_monfared <hasandoulabi@gmail.com>
2019-11-28 19:32:54 +0330

    add form login

--- Views/layout/footer.php
+++ Views/layout/footer.php
@@ -1,3 +1,4 @@
+</div>
 <script src="<?=URL."/libs/assest/js/jquery-3.4.1.min.js"?>"></script>
 <script src="<?=URL."/libs/assest/bootstrap/js/bootstrap.min.js"?>"></script>
 
---
+++ Views/user/login.php
@@ -0,0 +1,30 @@
+<div class="row">
+       
+   <div class="col"></div>
+    <div class="col mt-4 p-3 border border-primary">
+    <h1 class="text-center">Login</h1>
+
+    <form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group ">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+
+  <button type="submit" class="btn-block btn-primary">Login</button>
+</form>
+
+    
+    
+    
+    
+    </div>
+    <div class="col"></div>
+
+    </div>
+
+ 
