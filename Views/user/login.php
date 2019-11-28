3c70d5110dc1adff6a331673c0b271e02ab9e3fc
hassan_monfared <hasandoulabi@gmail.com>
2019-11-28 22:00:44 +0330

    add form login

--- Views/user/login.php
+++ Views/user/login.php
@@ -4,15 +4,15 @@
     <div class="col mt-4 p-3 border border-primary">
     <h1 class="text-center">Login</h1>
 
-    <form>
+    <form action="<?=URL."/user/checklogin"?>" method="POST">
   <div class="form-group">
     <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <small id="emailHelp" class="form-text  text-danger">We'll never share your email with anyone else.</small>
   </div>
   <div class="form-group ">
     <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
+    <input type="password" name="password" class="form-control" id="exampleInputPassword1">
   </div>
 
   <button type="submit" class="btn-block btn-primary">Login</button>
